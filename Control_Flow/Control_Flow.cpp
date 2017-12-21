7892ca512fd915d48d6d2fe82dde635c051724e7
anonymous.vn1985 <anonymous.vn1985@gmail.com>
2017-12-21 17:01:57 +0700

    New Commit

--- Control_Flow/Control_Flow.cpp
+++ Control_Flow/Control_Flow.cpp
@@ -2,10 +2,16 @@
 //
 
 #include "stdafx.h"
+#include "BinSearch.c"
 
+int binSearch(int x, int v[], int n);
 
 int main()
 {
+	int myArray[] = { 1, 3, 6, 9, 12, 17, 20, 22, 25, 30 };
+	int pos = binSearch(177, myArray, 10);
+	printf("%d\n", pos);
+
     return 0;
 }
 
